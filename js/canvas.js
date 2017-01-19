bbecb8ac83ffd6fc16128e12c164f38402c7db1c
sakizhu <0x0x7fffffff@gmail.com>
2017-01-19 17:59:22 +0800

    Update canvas.js

--- js/canvas.js
+++ js/canvas.js
@@ -1,5 +1,7 @@
 function drawCirleTimer()
 {
+	$('#circle_timer').attr('width', $('body').width());
+	$('#circle_timer').attr('height', $('#text_timer_div').height() * 4);
 	var x = $('#text_timer_div').offset().left + $('#text_timer_div').width() / 2;
 	var y = $('#text_timer_div').offset().top + $('#text_timer_div').height() / 2 - $('#show_btn').height();
 	drawColorCirle(x, y, 360, '#DBDBDB');
@@ -10,13 +12,13 @@ function drawColorCirle(x, y, degress, color)
 	var obj = document.getElementById('circle_timer');
 	var context = obj.getContext('2d');
 
-	var r1 = 50;
-	var r2 = r1 + 10;
+	var r1 = $('#text_timer_div').height() * 1.5;
+	var r2 = r1 + 5;
 	
 	
 	context.beginPath();
 	context.moveTo(x,y);
-	context.arc(x, y, 100, (-90) * Math.PI / 180, (degress - 90) * Math.PI / 180);
+	context.arc(x, y, r2, (-90) * Math.PI / 180, (degress - 90) * Math.PI / 180);
 	context.fillStyle = color;
 	context.strokeStyle = 'rgba(100, 100, 100, 0)';
 	context.stroke();
@@ -26,7 +28,7 @@ function drawColorCirle(x, y, degress, color)
 	
 	context.globalCompositeOperation = "destination-out";
     context.beginPath();
-    context.arc(x, y, 95, 0, Math.PI * 2);
+    context.arc(x, y, r1, 0, Math.PI * 2);
     context.fill();
     context.globalCompositeOperation = "source-over";
 }
